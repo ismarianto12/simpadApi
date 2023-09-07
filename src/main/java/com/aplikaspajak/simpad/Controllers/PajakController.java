a4ae7880028182aaaf146610a776d15cbc9b0026
Ismarianto Syafril <ismarianto.syafril@mncgroup.com>
2023-09-07 17:42:06 +0700

    update data

--- src/main/java/com/aplikaspajak/simpad/Controllers/PajakController.java
+++ src/main/java/com/aplikaspajak/simpad/Controllers/PajakController.java
@@ -1,5 +1,8 @@
 package com.aplikaspajak.simpad.Controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PajakController {
 
   @GetMapping
-  public String welcome() {
-    return "response java";
+  public ResponseEntity<Map<String, Object>> welcome() {
+    Map<String, Object> res = new HashMap<>();
+    res.put("v1", "status");
+    return ResponseEntity.ok(res);
   }
 
   @PostMapping
